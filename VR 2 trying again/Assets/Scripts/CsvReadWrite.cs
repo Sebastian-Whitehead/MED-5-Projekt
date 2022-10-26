5f462f8434e2e0daba192a2eef6a5e40b7eba820
Sebastian W <sebastianmikael2000@gmail.com>
2022-10-26 12:19:34 +0200

    Fixed formatting of cvs logging code

--- VR 2 trying again/Assets/Scripts/CsvReadWrite.cs
+++ VR 2 trying again/Assets/Scripts/CsvReadWrite.cs
@@ -5,69 +5,73 @@ using System.Text;
 using System.IO;
 using System;
 
-public class CsvReadWrite : MonoBehaviour {
-    
-    private List<string[]> rowData = new List<string[]>();
-   
+public class CsvReadWrite : MonoBehaviour
+{
+    private List<string[]> _rowData = new List<string[]>();
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         InvokeRepeating("Save", 0.5f, 0.5f);
     }
-    
-    void Save(){
 
+    // ReSharper disable Unity.PerformanceAnalysis
+    void Save()
+    {
         // Creating First row of titles manually..
-       string[] rowDataTemp = new string[3];
-      /*  rowDataTemp[0] = "Name";
-        rowDataTemp[1] = "ID";
-        rowDataTemp[2] = "Income";
-        rowData.Add(rowDataTemp);
-        */
+        string[] rowDataTemp = new string[3];
+        /*  rowDataTemp[0] = "Name";
+          rowDataTemp[1] = "ID";
+          rowDataTemp[2] = "Income";
+          rowData.Add(rowDataTemp);
+          */
 
         // You can add up the values in as many cells as you want.
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++)
+        {
             rowDataTemp = new string[90];
-           /* rowDataTemp[0] = "Sushanta"+i; // name
-            rowDataTemp[1] = ""+i; // ID */
-            rowDataTemp[2] = "$"+transform.position; // Income
-            rowData.Add(rowDataTemp);
+            /* rowDataTemp[0] = "Sushanta"+i; // name
+             rowDataTemp[1] = ""+i; // ID */
+            rowDataTemp[2] = "$" + transform.position; // Income
+            _rowData.Add(rowDataTemp);
         }
 
-        string[][] output = new string[rowData.Count][];
+        string[][] output = new string[_rowData.Count][];
 
-        for(int i = 0; i < output.Length; i++){
-            output[i] = rowData[i];
+        for (int i = 0; i < output.Length; i++)
+        {
+            output[i] = _rowData[i];
         }
 
-        int     length         = output.GetLength(0);
-        string     delimiter     = ",";
+        int length = output.GetLength(0);
+        const string delimiter = ",";
 
         StringBuilder sb = new StringBuilder();
-        
+
         for (int index = 0; index < length; index++)
             sb.AppendLine(string.Join(delimiter, output[index]));
-        
-        
-        string filePath = getPath();
+
+
+        string filePath = GetPath();
 
         StreamWriter outStream = System.IO.File.CreateText(filePath);
         outStream.WriteLine(sb);
         outStream.Close();
         Debug.Log(transform.position);
-      
     }
 
-    // Following method is used to retrive the relative path as device platform
-    private string getPath(){
-        #if UNITY_EDITOR
-        return Application.dataPath +"/CSV/"+"Saved_data.csv";
-        #elif UNITY_ANDROID
+    // Following method is used to retrieve the relative path as device platform
+    private string GetPath()
+    {
+#if UNITY_EDITOR
+        return Application.dataPath + "/CSV/" + "Saved_data.csv";
+#elif UNITY_ANDROID
         return Application.persistentDataPath+"Saved_data.csv";
-        #elif UNITY_IPHONE
+#elif UNITY_IPHONE
         return Application.persistentDataPath+"/"+"Saved_data.csv";
-        #else
+#else
         return Application.dataPath +"/"+"Saved_data.csv";
-        #endif
+#endif
     }
 }
