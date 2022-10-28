03c9080bbfafbef242e99de7c8707cc588985fd5
Charlotte1997AAU <80900771+Charlotte1997AAU@users.noreply.github.com>
2022-10-28 14:11:01 +0200

    Working tracking
    Well hopefully

--- VR 2 trying again/Assets/Scripts/CsvReadWrite.cs
+++ VR 2 trying again/Assets/Scripts/CsvReadWrite.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System;
+using Unity.VisualScripting;
 
 public class CsvReadWrite : MonoBehaviour
 {
@@ -14,15 +15,14 @@ public class CsvReadWrite : MonoBehaviour
     void Start()
     {
         First();
-        InvokeRepeating("Save", 4.0f, 4.0f);
+        InvokeRepeating("Save", 1.0f, repeatRate:1.0f);
+      
     }
-
 
     void First(){
         string[] rowDataTemp = new string[3];
-         rowDataTemp[0] = "New Participant";
-         _rowData.Add(rowDataTemp);
-          
+        rowDataTemp[0] = "New Participant";
+        _rowData.Add(rowDataTemp);
     }
     // ReSharper disable Unity.PerformanceAnalysis
     void Save()
@@ -36,7 +36,7 @@ public class CsvReadWrite : MonoBehaviour
           */
 
         // You can add up the values in as many cells as you want.
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 1; i++)
         {
             rowDataTemp = new string[3];
             /* rowDataTemp[0] = "Sushanta"+i; // name
@@ -44,30 +44,34 @@ public class CsvReadWrite : MonoBehaviour
             rowDataTemp[2] = "$" + transform.position; // Income
             _rowData.Add(rowDataTemp);
         }
-
-        string[][] output = new string[_rowData.Count][];
-
-        for (int i = 0; i < output.Length; i++)
-        {
-            output[i] = _rowData[i];
-        }
-
-        int length = output.GetLength(0);
-        const string delimiter = ",";
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int index = 0; index < length; index++)
-            sb.AppendLine(string.Join(delimiter, output[index]));
-
-
-        string filePath = GetPath();
-
-        StreamWriter outStream = System.IO.File.CreateText(filePath);
-        outStream.WriteLine(sb);
-        outStream.Close();
-        Debug.Log(transform.position);
     }
+
+    void OnApplicationQuit()
+         {
+
+             string[][] output = new string[_rowData.Count][];
+
+             for (int i = 0; i < output.Length; i++)
+             {
+                 output[i] = _rowData[i];
+             }
+
+             int length = output.GetLength(0);
+             const string delimiter = ",";
+
+             StringBuilder sb = new StringBuilder();
+
+             for (int index = 0; index < length; index++)
+                 sb.AppendLine(string.Join(delimiter, output[index]));
+
+
+             string filePath = GetPath();
+
+             StreamWriter outStream = System.IO.File.AppendText(filePath);
+             outStream.WriteLine(sb);
+             outStream.Close();
+             Debug.Log(transform.position);
+         }
 
     // Following method is used to retrieve the relative path as device platform
     private string GetPath()
