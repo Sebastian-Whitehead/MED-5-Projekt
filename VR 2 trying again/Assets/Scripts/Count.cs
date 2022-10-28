43038e0521ccb76ccfd1287566cc7c19f0869362
Tonko <tbosse20@student.aau.dk>
2022-10-28 11:11:31 +0200

    Counting using "sendMessage"

---
+++ VR 2 trying again/Assets/Scripts/Count.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Count : MonoBehaviour {
+    public void Counting(int number) {
+        Debug.Log(number);
+    }
+}
--- VR 2 trying again/Assets/Scripts/DestroyOnXRPickup.cs
+++ VR 2 trying again/Assets/Scripts/DestroyOnXRPickup.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOnXRPickup : MonoBehaviour
-{
-    public void DestroyGameObject()
-    {
+public class DestroyOnXRPickup : MonoBehaviour {
+    public void DestroyGameObject() {
         Destroy(gameObject, 1.5f);
         //Debug.Log("Destroyed Picked Up Object" after 1.5 seconds);
+
+        // // Count eggs destroyed
+        // TMP_Text counter = gameObject.Find("Counter").GetComponent<TMP_Text>();
+        // int currentCounter = int.Parse(counter.text);
+        // currentCounter++;
+        // counter.text = currentCounter.ToString();
     }
-    
 }
