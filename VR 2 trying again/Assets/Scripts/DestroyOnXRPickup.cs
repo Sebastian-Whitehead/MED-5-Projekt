7be97273cf1f04063e977eab991e572aa10ad003
Tonko <tbosse20@student.aau.dk>
2022-10-28 11:35:34 +0200

    Count up in script

--- VR 2 trying again/Assets/Scripts/DestroyOnXRPickup.cs
+++ VR 2 trying again/Assets/Scripts/DestroyOnXRPickup.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOnXRPickup : MonoBehaviour {
-    public gameObject counter;
 
     public void DestroyGameObject() {
-        counter.Count.countUp();
-        gameObject.Find(countiing).GetComponent<Count>().countUp();
+        GameObject.Find("Counter").GetComponent<Count>().countUp();
 
         Destroy(gameObject, 1.5f);
     }
