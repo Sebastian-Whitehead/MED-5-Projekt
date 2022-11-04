6cf2abd837fea1270f8488566f448a081c2d50b9
Tonko <tbosse20@student.aau.dk>
2022-11-04 14:05:37 +0100

    C-calculation and norm. Working but missing asp.

--- Statistic evaluation/statistics/main.R
+++ Statistic evaluation/statistics/main.R
@@ -1,18 +1,26 @@
 # https://www.geeksforgeeks.org/how-to-convert-csv-into-array-in-r/
+# tmp_participant = subset(tmp_participant, select=-c(w)) # Remove all w-values
 
+# TODO:
+# - Change label names in legend
+# - Normalize data
+# - Set width and height to 1,1
+
+# Importing libraries
 library(ggplot2)
+library(data.table)
 
-placementPath = "placement.csv"
-surveyPath = "survey.csv"
-rowLength = 4
-
-###############################################################
+# Static variables
+placementPath = "Nyt_tekstdokument.csv" # CSV-path for location data
+rowLength = 4 # Amount of columns
+surveyPath = "nyt_sruvey.csv" # CSV-path for survey data
+guardians = c("Meta", "Perma") # Guardian names
 
 ########## Get survey data ##########
 
 surveyData <- read.csv(surveyPath, header=FALSE) # Read data from file
 surveyData <- t(surveyData) # Transpose data
-surveyMean = colMeans(surveyData)
+surveyMean = colMeans(surveyData) # Mean columns
 
 ########## Get placement data ##########
 
@@ -20,40 +28,57 @@ placeData <- read.csv(placementPath, header=FALSE) # Read data from file
 placeData <- t(placeData) # Transpose data
 
 separatedList = list() # Initialize empty list for separated matrices
-meanLocation = list()
-for(i in 1:ncol(placeData)) {
+meanLocation = strtoi(list()) # Initizlize empty list for mean matrix
+
+for (i in 1:ncol(placeData)) {
   tmp_participant <- matrix(unlist(placeData[ , i]), nrow=rowLength) # Cut to matrix
-  rownames(tmp_participant) <- c("x", "y", "z", "w") # Column names
+  rownames(tmp_participant) <- c("X", "Y", "Z", "W") # Column names
   tmp_participant <- t(tmp_participant) # Transpose
-  # tmp_participant = subset(tmp_participant, select=-c(w)) # Remove all w-values
-  # meanLocation[[i]] = mean(data.table(tmp_participant)$w)
-  tmp_mean <- mean(data.table(tmp_participant)$w)
-  meanLocation <- rbind(meanLocation, c(tmp_mean, surveyMean[i]))
+  tmp_participant <- data.table(tmp_participant)
+
+  # Calculate the C-value for each row
+  tmp_participant = transform(
+    tmp_participant, # Chose participant
+    C=(sqrt(X*X + Z*Z) * W)) # Calculate C-value
+
+  # Normalize each C-value depending on participant
+  minC = min(tmp_participant$C) # Get minimum value of C
+  maxC = max(tmp_participant$C) # Get maxmimum value of C
+  tmp_participant$C = t((tmp_participant$C - minC) / (maxC - minC))
+
+  # Append C-mean and survey mean into a matrix
+  meanLocation <- rbind(
+    meanLocation,
+    c(round(mean(data.table(tmp_participant)$C), 10), # C-mean
+      round(surveyMean[i], 2), # Survey mean
+      i %% 2 + 1 # Un-/even participant
+    ))
   separatedList[[i]] <- data.table(tmp_participant) # Convert to data frame
 }
-
-separatedList
-
-colnames(meanLocation) <- c("x", "y") # Column names
-meanLocation
-
-# separatedList
-# separatedList[1]
-# data.frame(separatedList[1])$w
-# c(meanLocation)
-#
-# # Get mean location
-#
-# # Get mean of all w-values in one participant
-# mean(data.frame(separatedList[1])$w)
-#
-# mean
+colnames(meanLocation) <- c("Movement", "Immersion", "Guardian") # Column names
+meanLocation <- data.frame(meanLocation)
+meanLocation$Guardian <- as.factor(meanLocation$Guardian) # Integar Guardian-value
 
 ########## Show data ##########
 
-ggplot(data=data.frame(separatedList[2]), aes(x=x, y=y, size=z)) + geom_point()
+ggplot(
+  data=meanLocation,
+  aes(x=Movement, y=Immersion, color=Guardian)) +
+  geom_point(size=5) +
+  theme(legend.position="top",
+        legend.text=element_text(
+          size=13),
+        legend.background=element_rect(
+          size=0.5, linetype="solid",
+          color ="grey")) +
+  ylim(0, 7)
+  # labs(title = "#######",
+  #      subtitle = "#######",
+  #      caption = "#######")
 
-ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
-ggplot(meanLocation, aes(x, y)) + geom_point()
+########## Print data ##########
 
-# ggplot(data=data.frame(meanLocation), aes(x=x, y=y)) + geom_point() + scale_y_continuous(limits = c(0, 40))
+# separatedList
+# separatedList[1]
+# data.frame(separatedList[1])$W
+# mean(data.frame(separatedList[1])$W) # Get mean of all w-values in one participant
