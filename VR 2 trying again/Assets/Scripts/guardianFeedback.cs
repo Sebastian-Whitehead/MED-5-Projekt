9de13d937da3a3314eedb92ba493a5394c2e2265
Sebastian W <sebastianmikael2000@gmail.com>
2022-11-14 09:51:11 +0100

    Renamed and commented some code

--- VR 2 trying again/Assets/Scripts/guardianFeedback.cs
+++ VR 2 trying again/Assets/Scripts/guardianFeedback.cs
@@ -3,42 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class guardianFeedback : MonoBehaviour {
-    public Material staticMaterial;
-    public Material feedbackMaterial;
-    public float speed = 0.5f;
+    public Material staticMaterial;     //Standard Guardian Meterial   
+    public Material feedbackMaterial;   // Material for when the user is leaving the guardian space
+    public float speed = 0.5f;          // Texture interpolation speed
 
-    private bool alert = false;
-    private float lerp = 1f;
-    private Renderer rend;
+    private bool alert = false;         // Is there a tracking object outside the designated guardian
+    private Renderer rend;              // Guardian Render material
+    private int _trackObjsInside = 0;   // Number of tracking objects inside the guardian collider
     
-    private int insides = 0;
-
-    // Start is called before the first frame update
+    private float lerp = 1f;            // Interpolation value
+    
     void Start() {
         rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(insides);
+        //Debug.Log(_trackObjsInside);
         if (lerp < 1) {
             lerp += speed / 10;
             if (alert) {
-                rend.material.Lerp(staticMaterial, feedbackMaterial, lerp);
+                rend.material.Lerp(staticMaterial, feedbackMaterial, lerp); // Fade from standard to the feedback material
             } else {
-                rend.material.Lerp(feedbackMaterial, staticMaterial, lerp);
+                rend.material.Lerp(feedbackMaterial, staticMaterial, lerp); // Fade from feedback to standard material
             }
         }
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "TrackingObject")
+        if (other.gameObject.CompareTag("TrackingObject"))
         {
-            insides++;
-            
+            _trackObjsInside++;
             Debug.Log("Tracking object enter guardian");
-            
-            if (insides == 3)
+            if (_trackObjsInside == 3)
             {
                 alert = false;
                 lerp = 0f;
@@ -47,13 +44,11 @@ public class guardianFeedback : MonoBehaviour {
     }
 
     void OnTriggerExit(Collider other) {
-        if (other.gameObject.tag == "TrackingObject")
+        if (other.gameObject.CompareTag("TrackingObject"))
         {
-
-            insides--;
+            _trackObjsInside--;
             Debug.Log("Tracking object exiting guardian");
-
-            if (insides == 2)
+            if (_trackObjsInside == 2)
             {
                 alert = true;
                 lerp = 0f;
