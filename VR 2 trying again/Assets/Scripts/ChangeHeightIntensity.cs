49e3b47044424f446a91d5c0690acf25f711706b
Charlotte1997AAU <80900771+Charlotte1997AAU@users.noreply.github.com>
2022-11-23 14:37:47 +0100

    Height, not intensity tho
    Working bitch

--- VR 2 trying again/Assets/Scripts/ChangeHeightIntensity.cs
+++ VR 2 trying again/Assets/Scripts/ChangeHeightIntensity.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.XR;
+
 public class ChangeHeightIntensity : MonoBehaviour
 {
 
@@ -12,20 +13,86 @@ public class ChangeHeightIntensity : MonoBehaviour
     private float ValUpperLerpS = 0.01f;
     private float ValLowerLerpS = 0f;
     public GameObject Guardian;
-    private InputAction rightHandPrimaryButton;
-    
-    [SerializeField] private InputActionReference primaryButton;
+
+
     private void Start()
     {
-      
+     
     }
 
     // Update is called once per frame
     void Update(){
 
-      if (Input.GetKeyDown("primaryButton"))
+      var rightHandedControllers = new List<UnityEngine.XR.InputDevice>();
+      var desiredCharacteristicsright = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller;
+      UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(desiredCharacteristicsright, rightHandedControllers);
+
+
+      var leftHandedControllers = new List<UnityEngine.XR.InputDevice>();
+      var desiredCharacteristicsleft = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Left | UnityEngine.XR.InputDeviceCharacteristics.Controller;
+      UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(desiredCharacteristicsleft, leftHandedControllers);
+
+      bool leftprimaryval;
+      bool leftsecondaryval;
+      foreach (var device in leftHandedControllers)
       {
-        Debug.Log("Pls");
+          if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out leftprimaryval) && leftprimaryval)
+      {
+          Debug.Log("left primary button is pressed.");
+      
+      }
+        if(device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out leftsecondaryval) && leftsecondaryval){
+          Debug.Log("Left secondary bUtToN is pressed");
+        }
+      }
+
+      bool rightprimaryval;
+      bool rightsecondaryval;
+      foreach (var device in rightHandedControllers){
+        
+      if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out rightprimaryval) && rightprimaryval)
+      {
+          Debug.Log("right primary button is pressed.");
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpL", ValUpperLerpL);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpL", ValLowerLerpL);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpS", ValUpperLerpS);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpS", ValLowerLerpS);
+          
+          if (ValUpperLerpL >= 0.01f){
+              ValUpperLerpL -= 0.015f;
+          }
+          if(ValLowerLerpL >= 0f){
+            ValLowerLerpL -= 0.01f;
+          }
+          if(ValUpperLerpS >= 0.01f){
+            ValUpperLerpS -= 0.01f;
+          }
+          if(ValLowerLerpS >= 0f){
+            ValLowerLerpS -= 0.01f;
+          } 
+
+      }
+        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out rightsecondaryval) && rightsecondaryval)
+      {
+          Debug.Log("right secondary button is pressed.");
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpL", ValUpperLerpL);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpL", ValLowerLerpL);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpS", ValUpperLerpS);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpS", ValLowerLerpS);
+
+            if (ValUpperLerpL <= 2f){
+              ValUpperLerpL += 0.015f;
+            }
+            if (ValLowerLerpL <= 1f){
+              ValLowerLerpL +=0.01f;
+            } 
+            if (ValUpperLerpS <= 2f){
+              ValUpperLerpS += 0.01f; 
+            }
+            if (ValLowerLerpS <= 1f){
+              ValLowerLerpS += 0.01f;
+            }
+      }
       }
 
       if(Input.GetKeyDown("up")){
@@ -71,6 +138,4 @@ public class ChangeHeightIntensity : MonoBehaviour
       
     }
 
-  
-  
 }
