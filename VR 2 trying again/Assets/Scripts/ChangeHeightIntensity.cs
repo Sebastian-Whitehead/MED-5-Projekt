a47c757188c9f4687a656c6eda17327cb0b6b61b
Charlotte1997AAU <80900771+Charlotte1997AAU@users.noreply.github.com>
2022-11-25 09:08:43 +0100

    Working with sticks
    "MaYbE We CoULd UsE ThE StICks"
    - Whitehead
    <3

--- VR 2 trying again/Assets/Scripts/ChangeHeightIntensity.cs
+++ VR 2 trying again/Assets/Scripts/ChangeHeightIntensity.cs
@@ -12,6 +12,11 @@ public class ChangeHeightIntensity : MonoBehaviour
     private float ValLowerLerpL = -0.463f; 
     private float ValUpperLerpS = 0.01f;
     private float ValLowerLerpS = 0f;
+
+    private float ValTransL = 0.179f;
+
+    private float ValTransS = 0.136f;
+
     public GameObject Guardian;
 
 
@@ -34,106 +39,100 @@ public class ChangeHeightIntensity : MonoBehaviour
 
       bool leftprimaryval;
       bool leftsecondaryval;
+      Vector2 leftstick;
+      
       foreach (var device in leftHandedControllers)
       {
-          if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out leftprimaryval) && leftprimaryval)
-      {
+        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out leftstick)){
+        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_TransL", ValTransL);
+        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_TransS", ValTransS);
+        float yvalue = leftstick.y;
+        //Modifies the Transparency Values for TransL
+        ValTransL += yvalue/10;
+        if (ValTransL >= 1f){
+          ValTransL = 1f;
+        }
+        if (ValTransL <= 0f){
+          ValTransL = 0f;
+        }
+
+        //Modifies the transparency values for TransS
+        ValTransS += yvalue/10;
+        if (ValTransS >= 1f){
+          ValTransS = 1f;
+        }
+        if (ValTransS <= 0f){
+          ValTransS = 0f;
+        }
+
+    }
+
+          if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out leftprimaryval) && leftprimaryval){
           Debug.Log("left primary button is pressed.");
-      
-      }
+        }
+
         if(device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out leftsecondaryval) && leftsecondaryval){
-          Debug.Log("Left secondary bUtToN is pressed");
+        Debug.Log("Left secondary bUtToN is pressed");
         }
       }
 
       bool rightprimaryval;
       bool rightsecondaryval;
+      Vector2 rightstick; 
       foreach (var device in rightHandedControllers){
+
+       if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out rightstick)){
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpL", ValUpperLerpL);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpL", ValLowerLerpL);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpS", ValUpperLerpS);
+          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpS", ValLowerLerpS);
+          float yvalue = rightstick.y;
         
+          ValUpperLerpL += yvalue/10;
+          if (ValUpperLerpL >= 2){
+            ValUpperLerpL= 2f;
+          }
+          if (ValUpperLerpL <= 0.01f){
+            ValUpperLerpL = 0.015f;
+          }
+
+          ValLowerLerpL += yvalue/10;
+          if (ValUpperLerpL >= 1){
+            ValUpperLerpL= 1f;
+          }
+          if (ValLowerLerpL <= 0f){
+            ValUpperLerpL = 0.01f;
+          }
+
+          ValUpperLerpS += yvalue/10;
+          if (ValUpperLerpS >= 2){
+            ValUpperLerpS= 2f;
+          }
+          if (ValUpperLerpS <= 0.01f){
+            ValUpperLerpS = 0.01f;
+          }
+
+          ValLowerLerpS += yvalue/10;
+          if (ValUpperLerpS >= 1){
+            ValUpperLerpS= 1f;
+          }
+          if (ValLowerLerpS <=0f){
+            ValUpperLerpS = 0.01f;
+          }
+
+       
+       
+       
+       }
+
       if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out rightprimaryval) && rightprimaryval)
       {
           Debug.Log("right primary button is pressed.");
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpL", ValUpperLerpL);
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpL", ValLowerLerpL);
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpS", ValUpperLerpS);
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpS", ValLowerLerpS);
-          
-          if (ValUpperLerpL >= 0.01f){
-              ValUpperLerpL -= 0.015f;
-          }
-          if(ValLowerLerpL >= 0f){
-            ValLowerLerpL -= 0.01f;
-          }
-          if(ValUpperLerpS >= 0.01f){
-            ValUpperLerpS -= 0.01f;
-          }
-          if(ValLowerLerpS >= 0f){
-            ValLowerLerpS -= 0.01f;
-          } 
-
       }
-        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out rightsecondaryval) && rightsecondaryval)
+      if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out rightsecondaryval) && rightsecondaryval)
       {
-          Debug.Log("right secondary button is pressed.");
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpL", ValUpperLerpL);
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpL", ValLowerLerpL);
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpS", ValUpperLerpS);
-          Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpS", ValLowerLerpS);
-
-            if (ValUpperLerpL <= 2f){
-              ValUpperLerpL += 0.015f;
-            }
-            if (ValLowerLerpL <= 1f){
-              ValLowerLerpL +=0.01f;
-            } 
-            if (ValUpperLerpS <= 2f){
-              ValUpperLerpS += 0.01f; 
-            }
-            if (ValLowerLerpS <= 1f){
-              ValLowerLerpS += 0.01f;
-            }
+          
       }
-      }
-
-      if(Input.GetKeyDown("up")){
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpL", ValUpperLerpL);
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpL", ValLowerLerpL);
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpS", ValUpperLerpS);
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpS", ValLowerLerpS);
-
-          if (ValUpperLerpL <= 2f){
-            ValUpperLerpL += 0.015f;
-          }
-          if (ValLowerLerpL <= 1f){
-            ValLowerLerpL +=0.01f;
-          } 
-          if (ValUpperLerpS <= 2f){
-            ValUpperLerpS += 0.01f; 
-          }
-          if (ValLowerLerpS <= 1f){
-            ValLowerLerpS += 0.01f;
-          }
-
-        }
-
-        if(Input.GetKeyDown("down")){
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpL", ValUpperLerpL);
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpL", ValLowerLerpL);
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_UpperLerpS", ValUpperLerpS);
-        Guardian.GetComponent<Renderer>().sharedMaterial.SetFloat("_LowerLerpS", ValLowerLerpS);
-        
-        if (ValUpperLerpL >= 0.01f){
-            ValUpperLerpL -= 0.015f;
-        }
-        if(ValLowerLerpL >= 0f){
-          ValLowerLerpL -= 0.01f;
-        }
-        if(ValUpperLerpS >= 0.01f){
-          ValUpperLerpS -= 0.01f;
-        }
-        if(ValLowerLerpS >= 0f){
-          ValLowerLerpS -= 0.01f;
-        } 
       }
       
     }
