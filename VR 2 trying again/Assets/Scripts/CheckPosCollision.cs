15cf716d7e0e7895339c25cc767efa83bae621de
Sebastian W <sebastianmikael2000@gmail.com>
2022-12-07 09:49:25 +0100

    Comment Code

--- VR 2 trying again/Assets/Scripts/CheckPosCollision.cs
+++ VR 2 trying again/Assets/Scripts/CheckPosCollision.cs
@@ -6,33 +6,33 @@ using UnityEngine;
 
 public class CheckPosCollision : MonoBehaviour
 {
-    private Renderer _renderer;
-    public GameObject target;
-    private bool _collisionState = false;
+    private Renderer _renderer;           // This game object's renderer
+    public GameObject target;             // Controller game object
+    private bool _collisionState = false; // Current collision state
 
     private void Start()
     {
-        _renderer = GetComponent<Renderer>();
+        _renderer = GetComponent<Renderer>();   // Get the object renderer
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.name == target.name)
+        if (col.name == target.name)            // If the target colliding object is the target object
         {
             //Debug.Log(" Correct collision!");
-            _renderer.material.color = Color.green;
-            _collisionState = true;
+            _renderer.material.color = Color.green; // change this object's color to green 
+            _collisionState = true;                 // Set the current collision state to true
         }
     }
 
-    private void OnTriggerExit(Collider col)
+    private void OnTriggerExit(Collider col)   // When leaving the collider
     {
-        _renderer.material.color = Color.red;
-        _collisionState = false;
+        _renderer.material.color = Color.red;  // Set color to red
+        _collisionState = false;               // set collision state to false
     }
 
-    public bool CheckCollision()
+    public bool CheckCollision()               // Getter function to be called by external game - objects
     {
-        return _collisionState;
+        return _collisionState;                 
     }
 }
